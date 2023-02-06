f32f6796412e1c73dee8def4ebe6feb565cb488d
Titus Wormer <tituswormer@gmail.com>
2023-02-06 16:17:06 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -7,19 +7,30 @@
 const severities = {true: 2, false: 1, null: 0, undefined: 0}
 
 /**
+ * Sort messages in the given vfile.
+ *
  * @template {VFile} File
+ *   File type.
  * @param {File} file
+ *   File to sort.
  * @returns {File}
+ *   Sorted file.
  */
+// To do: next major: don’t return `file`.
 export function sort(file) {
   file.messages.sort(comparator)
   return file
 }
 
 /**
+ * Compare a message.
+ *
  * @param {VFileMessage} a
+ *   Left message.
  * @param {VFileMessage} b
+ *   Right message.
  * @returns {number}
+ *   Order.
  */
 function comparator(a, b) {
   return (
@@ -34,10 +45,16 @@ function comparator(a, b) {
 }
 
 /**
+ * Compare a numeric field.
+ *
  * @param {VFileMessage} a
+ *   Left message.
  * @param {VFileMessage} b
+ *   Right message.
  * @param {'column' | 'line'} field
+ *   Numeric field.
  * @returns {number}
+ *   Order.
  */
 function check(a, b, field) {
   return (a[field] || 0) - (b[field] || 0)
@@ -45,9 +62,13 @@ function check(a, b, field) {
 
 /**
  * @param {VFileMessage} a
+ *   Left message.
  * @param {VFileMessage} b
+ *   Right message.
  * @param {'reason' | 'ruleId' | 'source'} field
+ *   String field.
  * @returns {number}
+ *   Order.
  */
 function compare(a, b, field) {
   return String(a[field] || '').localeCompare(b[field] || '')
