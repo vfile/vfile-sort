6e2516af8ecfd1d8cb815dd8b30c678bd9625d6e
Titus Wormer <tituswormer@gmail.com>
2023-06-10 11:30:39 +0200

    Update `vfile`

--- test.js
+++ test.js
@@ -12,32 +12,31 @@ test('sort', async function () {
 
   let file = new VFile()
 
-  // @ts-expect-error runtime.
-  file.message('Hotel', {column: 0})
+  file.message('Hotel', {line: 0, column: 0})
   file.message('Foxtrot')
-  // @ts-expect-error runtime.
-  file.message('Alpha', {line: 3})
+  file.message('Alpha', {line: 3, column: 0})
   file.message('Bravo', {line: 3, column: 1})
   file.message('Charlie', {line: 3, column: 2})
   file.message('Delta', {line: 0, column: 1})
-  // @ts-expect-error runtime.
-  file.message('Echo', {column: 1})
-  // @ts-expect-error runtime.
-  file.message('Golf', {line: 0})
-  // @ts-expect-error runtime.
-  file.message('Golf', {line: 0})
+  file.message('Echo', {line: 0, column: 1})
+  file.message('Golf', {line: 0, column: 0})
+  file.message('Golf', {line: 0, column: 0})
 
-  assert.deepEqual(sort(file).messages.map(String), [
-    '1:1: Foxtrot',
-    '1:1: Golf',
-    '1:1: Golf',
-    '1:1: Hotel',
-    '1:1: Delta',
-    '1:1: Echo',
-    '3:1: Alpha',
-    '3:1: Bravo',
-    '3:2: Charlie'
-  ])
+  assert.deepEqual(
+    sort(file).messages.map(String),
+    [
+      '1:1: Foxtrot',
+      '1:1: Golf',
+      '1:1: Golf',
+      '1:1: Hotel',
+      '1:1: Delta',
+      '1:1: Echo',
+      '3:1: Alpha',
+      '3:1: Bravo',
+      '3:2: Charlie'
+    ],
+    'should compare on line/column, reason'
+  )
 
   file = new VFile()
 
@@ -48,9 +47,9 @@ test('sort', async function () {
     file.fail('Three', {line: 2, column: 5})
   } catch {}
 
-  assert.deepEqual(sort(file).messages.map(String), [
-    '2:5: Three',
-    '2:5: Two',
-    '2:5: One'
-  ])
+  assert.deepEqual(
+    sort(file).messages.map(String),
+    ['2:5: Three', '2:5: Two', '2:5: One'],
+    'should compare on `fatal`'
+  )
 })
